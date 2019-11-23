b94bc44dc26e2e19ac11170dd5bb3273fb22087d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-11-23 09:27:08 +0000

    just fixed that

--- lurlene/bridge.py
+++ lurlene/bridge.py
@@ -97,7 +97,6 @@ class LiveCodingBridge:
                 if oldspeed != self.context.speed:
                     frameindex = (frameindex - self.bias) / oldspeed * self.context.speed + self.bias
                 if oldsections != self.context.sections:
-                    # FIXME: Where is the bias!
                     frameindex = self._adjustframeindex(Sections(self.context.speed, oldsections), frameindex)
 
     def _adjustframeindex(self, oldsections, frameindex):
